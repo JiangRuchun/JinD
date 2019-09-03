b7f7587e93f2a1b011bb8eb52a169d8952a50186
Jiang <18482167212@163.com>
2019-09-03 10:48:14 +0800

    1.添加首页home 2.添加小店页shop 3.添加路由跳转

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule,
